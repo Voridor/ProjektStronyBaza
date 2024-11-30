b658dfe4d394f6109eb39f33ce4cb650fcc9fad2
Kacper Porada <126320743+Voridor@users.noreply.github.com>
2024-11-30 18:47:49 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <React.Fragment>
+        <div id="mainBar">
+          <p>MainBar</p>
+          <ul id="gatunki">
+            <li>Kryminały</li>
+            <li>Historyczne</li>
+            <li>Popularonaukowe</li>
+            <li>Horror</li>
+            <li>Komiksy</li>
+          </ul>
+        </div>
+        <div id="books">
+          <div className="book">
+            <p>XD</p>
+          </div>
+          <div className="book">
+            <p>XD</p>
+          </div>
+          <div className="book">
+            <p>XD</p>
+          </div>
+          <div className="book">
+            <p>XD</p>
+          </div>
+          <div className="book">
+            <p>XD</p>
+          </div>
+          <div className="book">
+            <p>XD</p>
+          </div>
+          <div className="book">
+            <p>XD</p>
+          </div>
+          <div className="book">
+            <p>XD</p>
+          </div>
+          <div className="book">
+            <p>XD</p>
+          </div>
+          <div className="book">
+            <p>XD</p>
+          </div>
+        </div>
+      </React.Fragment>
+  );
+}
+
+export default App;
